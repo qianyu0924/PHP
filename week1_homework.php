4aaeb3cdc967bc298872dd5523c0d52f5c065f71
qianyu0924 <113584594+qianyu0924@users.noreply.github.com>
2023-02-20 21:17:51 +0800

    Add files via upload

---
+++ week1_homework.php
@@ -0,0 +1,27 @@
+<HTML>
+    <HEAD>
+        <meta charset="UTF-8">
+        <title>第一週作業</title>
+    </HEAD>
+
+    <body>
+        Hello PHP!<br/>
+        
+        <?php
+
+        echo "Welcome to PHP world";
+        $x=20;
+        $y=3;
+        $i=$x/$y;
+        $j=$x%$y;
+        echo "<br/>";
+        echo "20除以3等於";
+        echo $i;
+        echo "<br/>";
+        echo "20除以3的餘數等於";
+        echo $j;
+
+        ?>
+        
+    </body>
+</HTML>  
